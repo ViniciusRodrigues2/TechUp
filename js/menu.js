7392690efc27629fa26aadb2e17e35c6f74d0e32
Code-Space <100551018+ViniciusRodrigues2@users.noreply.github.com>
2022-05-11 02:42:55 -0300

    commit inicial
    site em modo de teste

---
+++ js/menu.js
@@ -0,0 +1,10 @@
+let show = 0;
+
+let menuMobile = document.querySelector('.menu-mobile');
+function toogle(){
+    let nav = document.querySelector('.menu-container');
+    document.body.style.overflow = show ? "hidden" : "initial"
+    nav.classList.toggle('active',show);
+    show = !show;
+}
+menuMobile.addEventListener('click',toogle);
