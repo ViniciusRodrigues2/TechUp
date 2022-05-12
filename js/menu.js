eca8f0213fbb394c649745e9d3ac2c4c94f2bbe7
Code-Space <100551018+ViniciusRodrigues2@users.noreply.github.com>
2022-05-12 01:58:14 -0300

    commit inicial
    testando...

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,12 @@
 let show = 0;
 
 let menuMobile = document.querySelector('.menu-mobile');
-function toogle(){
+function toogle(event){
+    if(event.type === 'touchstart') event.preventDefault
     let nav = document.querySelector('.menu-container');
     document.body.style.overflow = show ? "hidden" : "initial"
     nav.classList.toggle('active',show);
     show = !show;
 }
 menuMobile.addEventListener('click',toogle);
+menuMobile.addEventListener('touchstart',toogle)
